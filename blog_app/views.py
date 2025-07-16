3b2a47e431decb298d8ab1d7c0262de207466729
manjurai22 <manjurai611@gmail.com>
2025-07-16 21:23:18 +0545

    commit

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog_app.models import Post
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_at__isnull=False)
@@ -15,10 +16,19 @@ def post_detail(request, pk):
         "post_detail.html",
         {"post": post},
         )
+@login_required
 def draft_list(request):
     posts = Post.objects.filter(published_at__isnull=True)
     return render(
         request,
         "draft_list.html",
         {"posts": posts},
+        )
+@login_required
+def draft_detail(request, pk):
+    post = Post.objects.get(pk=pk, published_at__isnull=True)
+    return render(
+        request,
+        "draft_detail.html",
+        {"post": post},
         )
