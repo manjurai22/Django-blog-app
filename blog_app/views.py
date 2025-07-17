f0e528e7050e0e6c1fd1122c5ee1874d100a4fbb
manjurai22 <manjurai611@gmail.com>
2025-07-17 18:09:18 +0545

    form validation

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,34 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from blog_app.models import Post
 from django.contrib.auth.decorators import login_required
+from blog_app.forms import PostForm
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_at__isnull=False)
-    return render(
-        request,
-        "post_list.html",
-        {"posts": posts},
-        )
+    return render(request, "post_list.html",{"posts": posts})
 def post_detail(request, pk):
     post =Post.objects.get(pk=pk)
-    return render(
-        request,
-        "post_detail.html",
-        {"post": post},
-        )
+    return render(request, "post_detail.html",{"post": post})
 @login_required
 def draft_list(request):
     posts = Post.objects.filter(published_at__isnull=True)
-    return render(
-        request,
-        "draft_list.html",
-        {"posts": posts},
-        )
+    return render(request, "draft_list.html", {"posts": posts})
 @login_required
 def draft_detail(request, pk):
     post = Post.objects.get(pk=pk, published_at__isnull=True)
-    return render(
-        request,
-        "draft_detail.html",
-        {"post": post},
-        )
+    return render(request, "draft_detail.html", {"post": post})
+
+@login_required
+def post_create(request):
+    if request.method == "GET":
+        form = PostForm()
+        return render(request, "post_create.html", {"form": form})
+    else:
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("draft-detail", pk=post.pk)
+
+        else:
+            return render(request, "post_create.html", {"form": form})
