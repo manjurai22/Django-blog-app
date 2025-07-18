74779e341bec6bb7dc9c4ce777bfad986569428e
manjurai22 <manjurai611@gmail.com>
2025-07-18 17:38:25 +0545

    publishing post

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("draft-list/",views.draft_list, name="draft-list"),
     path("draft-detail/<int:pk>/",views.draft_detail, name="draft-detail"),
     path("post-create/",views.post_create, name="post-create"),
+    path("post-update/<int:pk>",views.post_update,name="post-update"),
+    path("post-publish/<int:pk>/",views.draft_publish,name="draft-publish"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -5,19 +5,35 @@ from blog_app.forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_at__isnull=False)
-    return render(request, "post_list.html",{"posts": posts})
+    posts = Post.objects.filter(published_at__isnull=False).order_by("-published_at")
+    return render(
+        request,
+        "post_list.html",
+        {"posts": posts},
+        )
 def post_detail(request, pk):
     post =Post.objects.get(pk=pk)
-    return render(request, "post_detail.html",{"post": post})
+    return render(
+        request,
+        "post_detail.html",
+        {"post": post},
+        )
 @login_required
 def draft_list(request):
     posts = Post.objects.filter(published_at__isnull=True)
-    return render(request, "draft_list.html", {"posts": posts})
+    return render(
+        request,
+        "draft_list.html",
+        {"posts": posts},
+        )
 @login_required
 def draft_detail(request, pk):
     post = Post.objects.get(pk=pk, published_at__isnull=True)
-    return render(request, "draft_detail.html", {"post": post})
+    return render(
+        request,
+        "draft_detail.html",
+        {"post": post},
+        )
 
 @login_required
 def post_create(request):
@@ -35,3 +51,46 @@ def post_create(request):
 
         else:
             return render(request, "post_create.html", {"form": form})
+
+@login_required
+def post_update(request,pk):
+    if request.method =="GET":
+        post=Post.objects.get(pk=pk)
+        form=PostForm(instance=post)
+        return render(
+            request,
+            "post_create.html",
+            {"form":form},
+        )
+    else:
+        post=Post.objects.get(pk=pk)
+        form= PostForm(request.POST,instance=post)
+        if form.is_valid():
+            post=form.save()
+            if post.published_at:
+                return redirect("post-detail",post.pk)
+            else:
+                return redirect("draft-detail",post.pk)
+        else:
+            return render(
+                request,
+                "post_create.html",
+                {"form": form},
+            )
+
+from django.utils import timezone   #Module level import not at top of file
+@login_required
+def draft_publish(request,pk):
+    post=Post.objects.get(pk=pk,published_at__isnull=True)
+    post.published_at=timezone.now()
+    post.save()
+    return redirect("post-list")
+
+@login_required
+def post_delete(request,pk):
+    post=Post.objects.get(pk=pk)
+    post.delete()
+    if post.published_at:
+        return redirect("post-list")
+    else:
+        return redirect("draft-list")
